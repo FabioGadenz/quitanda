9e29bd8bf03a26c635cae40546b633a0b82c43b7
FABIO GADENZ <fabio.gadenz@gmail.com>
2022-04-13 21:54:19 -0300

    Alteração  para Getx
    alterado biblioteca para Getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'src/pages/auth/sign_in_screen.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Quitanda',
       theme: ThemeData(
 
